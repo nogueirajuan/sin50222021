d367bef5516a6f0b299c8fcb436bf83a8e5c62eb
Juan Nogueira <juan.nogueira@ifood.com.br>
2021-06-29 15:49:40 -0300

    tests: IdentifierTest

--- src/test/java/codigos/IdentifierTest.java
+++ src/test/java/codigos/IdentifierTest.java
@@ -1,14 +1,39 @@
 package codigos;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class IdentifierTest {
 
     @Test
-    public void testIDValido(){
-        Assert.assertEquals(true,Identifier.validaIdentificador("abc"));
+    public void testIDValido() {
+        assertTrue(Identifier.validaIdentificador("abc"));
     }
 
+    @Test
+    public void identificadorNulo() {
+        assertFalse(Identifier.validaIdentificador(null));
+    }
 
+    @Test
+    public void identificadorVazio() {
+        assertFalse(Identifier.validaIdentificador(""));
+    }
+
+    @Test
+    public void identificadorExcessoTamanho() {
+        assertFalse(Identifier.validaIdentificador("abc1234567"));
+    }
+
+    @Test
+    public void identificadorInvalidoIniciandoComNumero() {
+        assertFalse(Identifier.validaIdentificador("1abc"));
+    }
+
+    @Test
+    public void identificadorInvalidoIniciandoComCaracterEspecial() {
+        assertFalse(Identifier.validaIdentificador("@abc12"));
+    }
 }
