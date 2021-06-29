bfffb17ff03e8efb9e52d26ec9f130faaf465c0e
Juan Nogueira <juan.nogueira@ifood.com.br>
2021-06-28 23:25:13 -0300

    config: java 8

--- src/test/java/codigos/WordsTest.java
+++ src/test/java/codigos/WordsTest.java
@@ -1,4 +1,17 @@
 package codigos;
 
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class WordsTest {
+
+    @Test
+    public void validaPalavraNula(){
+        String palavra = null;
+        Words words = new Words();
+        int response = words.countWords(palavra);
+        assertEquals(response, -1);
+    }
+
 }
